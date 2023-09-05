867d9252fc8d5cf0299f98440448cb75fb7a2aa1
yun <dbstjdgur78@naver.com>
2023-09-05 21:54:51 +0900

    feat: 전화번호 설계

--- src/main/java/phon/Main.java
+++ src/main/java/phon/Main.java
@@ -1,8 +1,7 @@
 package phon;
 
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Main {
 
@@ -18,6 +17,11 @@ public class Main {
             numbers.add(number);
         }
 
+        public boolean hasPhoneNumber(PhoneNumber number){
+            return numbers.contains(number);
+        }
+
+
         @Override
         public String toString() {
             return "Person{" +
@@ -25,6 +29,7 @@ public class Main {
                     ", numbers=" + numbers +
                     '}';
         }
+
     }
 
     private static class PhoneNumber{
@@ -34,18 +39,57 @@ public class Main {
             this.phoneNumber = rawPhoneNumber.replaceAll("[^0-9]","");
         }
         @Override
+        public boolean equals(Object o){
+            if(!(o instanceof PhoneNumber)) return false;
+            return phoneNumber.equals(((PhoneNumber) o).phoneNumber);
+        }
+        @Override
         public String toString(){
             return "PhoneNumber{" + "phoneNumber='" + phoneNumber + '\'' + '}';
         }
     }
 
+    private static class PhoneBook{
+        private final Set<Person> people;
 
+        private PhoneBook(){
+            people = new HashSet<>();
+        }
+
+        public void addPerson(Person person){
+            people.add(person);
+        }
+
+        public Person search(PhoneNumber number){
+            return people.stream().filter(p -> p.hasPhoneNumber(number)).findFirst().orElse(null);
+        }
+        @Override
+        public String toString() {
+            return "PhoneBook{" +
+                    "people=" + people +
+                    '}';
+        }
+    }
 
     public static void main(String[] args) {
         Person person = new Person("윤성혁");
         person.addPhoneNumber(new PhoneNumber("010-3487-6544"));
-        person.addPhoneNumber(new PhoneNumber("010 3487 6544"));
-        person.addPhoneNumber(new PhoneNumber("01034876544"));
-        System.out.println(person);
+        person.addPhoneNumber(new PhoneNumber("010-2579-6544"));
+
+        Person person2 = new Person("김철수");
+        person2.addPhoneNumber(new PhoneNumber("010-2468-0246"));
+
+        Person person3 = new Person("김영희");
+        person3.addPhoneNumber(new PhoneNumber("010-1357-9135"));
+
+        PhoneBook phoneBook = new PhoneBook();
+
+        phoneBook.addPerson(person);
+        phoneBook.addPerson(person2);
+        phoneBook.addPerson(person3);
+        System.out.println(phoneBook.search(new PhoneNumber("01034876544")));
+        System.out.println(phoneBook.search(new PhoneNumber("01034876542")));
+
+        System.out.println(phoneBook);
     }
 }
